a1d128633fb3c31669fd84a636a01b7e4242cab0
paschalidi <paschalidis@factor-e.eu>
2016-12-06 11:57:59 +0200

    master: Deletes Testing container

--- app/containers/Testing/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-import expect from 'expect';
-import testingReducer from '../reducer';
-import { fromJS } from 'immutable';
-
-describe('testingReducer', () => {
-  it('returns the initial state', () => {
-    expect(testingReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
